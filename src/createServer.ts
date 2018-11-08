d4dcd3257756b654cfe5cc09801aa45b6618f519
Tomasz Kawik <tomasz.kawik@debijenkorf.nl>
2018-11-08 21:41:33 +0100

    Fix schema path. Add fake data.

--- src/createServer.ts
+++ src/createServer.ts
@@ -1,19 +1,16 @@
 import { GraphQLServer } from 'graphql-yoga';
-import Mutation from './resolvers/Mutation';
-import Query from './resolvers/Query';
-import db from './db';
+import {Mutation} from './resolvers/Mutation';
+import {Query} from './resolvers/Query';
+import {db} from './db';
 
 // Create the GraphQL Yoga Server
 
 export function createServer() {
     return new GraphQLServer({
-        typeDefs: 'src/schema.graphql',
+        typeDefs: './src/schema.graphql',
         resolvers: {
             Mutation,
             Query,
-        },
-        resolverValidationOptions: {
-            requireResolversForResolveType: false,
         },
         context: req => ({ ...req, db }),
     });
--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,2 +1,2 @@
-export const Mutations = {};
+export const Mutation = {};
 
